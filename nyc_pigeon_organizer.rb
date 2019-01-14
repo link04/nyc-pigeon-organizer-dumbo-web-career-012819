b45224594d9d372662f1a73d4b104716048db366
Maximo Bautista <max.bautista97@gmail.com>
2019-01-14 00:26:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_data = {}
+  data.each do |key, value|
+      value.each do |intel, pigeons_names|
+        pigeons_names.each do |pingeon| 
+          if !organized_data[pingeon]
+            organized_data[pingeon] = {}
+            organized_data[pingeon][key] = []
+            organized_data[pingeon][key] << intel.to_s
+          elsif organized_data[pingeon] && !organized_data[pingeon][key] 
+            organized_data[pingeon][key] = []
+            organized_data[pingeon][key] << intel.to_s
+          elsif organized_data[pingeon] && organized_data[pingeon][key] 
+            organized_data[pingeon][key] << intel.to_s
+          end
+        end
+      end
+  end
+  organized_data
 end
