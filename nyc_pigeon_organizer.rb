9a3ed357b45cad0d1a636e61d2abb721810c0cee
Maximo Bautista <max.bautista97@gmail.com>
2019-01-14 00:10:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,8 +12,8 @@ def nyc_pigeon_organizer(data)
              organized_data[pingeon][key] << intel
           elsif organized_data[pingeon]
             organized_data[pingeon][key] = []
-             organized_data[pingeon][key] << intel
-          
+            organized_data[pingeon][key] << intel
+          end
         
           
         end
